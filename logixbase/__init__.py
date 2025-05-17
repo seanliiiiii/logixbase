7465ccb72c1486f7014b3b629e54c4fe374f2208
seanliiiii <339640170@qq.com>
2025-05-17 23:45:58 +0800

    Auto build and publish

--- logixbase/__init__.py
+++ logixbase/__init__.py
@@ -1,10 +1,10 @@
 # LogixBase - 高度模块化的量化交易策略开发框架
 # 版本信息
-__version__ = '0.1.2'
+__version__ = '0.1.5'
 __author__ = 'Sean'
 __email__ = 'trader@logixquant.com'
 
 # 导出常用模块，方便用户导入
-from logixbase.engine import BaseEngine
-from logixbase.plugin.base import BasePlugin
-from logixbase.engine import BaseComponent
+from .engine import BaseEngine
+from .plugin.base import BasePlugin
+from .engine import BaseComponent
